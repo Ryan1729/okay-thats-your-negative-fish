b4b480a390b8221919664435f39660bd5859de75
Ryan1729 <Ryan1729@gmail.com>
2017-02-19 02:27:48 -0700

    own the event loop and semi-actually-implement draw hexagon
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,19 @@
 extern crate sdl2;
 
-use platform;
+use platform::Platform;
 
 pub fn go() {
-    platform::init();
+    let mut platform = Platform::new();
 
-    platform::draw_hexagon(10, 10);
+    platform.draw_hexagon(10, 10);
+
+    'running: loop {
+        let should_quit = platform.quit_on_keypress();
+
+        if should_quit {
+            break 'running;
+        }
+
+        // The rest of the game loop goes here...
+    }
 }
--- src/platform.rs
+++ src/platform.rs
@@ -4,52 +4,66 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Point;
+use sdl2::render::Renderer;
+use sdl2::EventPump;
 
-pub fn init() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+pub struct Platform<'a> {
+    renderer: Renderer<'a>,
+    event_pump: EventPump,
+}
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
+impl<'a> Platform<'a> {
+    pub fn new() -> Self {
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
 
-    let mut renderer = window.renderer().build().unwrap();
+        let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+            .position_centered()
+            .opengl()
+            .build()
+            .unwrap();
 
-    // renderer.set_scale(10f32, 10f32).unwrap();
+        let mut renderer = window.renderer().build().unwrap();
 
-    renderer.set_draw_color(Color::RGB(250, 224, 55));
-    renderer.clear();
+        renderer.set_draw_color(Color::RGB(250, 224, 55));
+        renderer.clear();
 
-    // cr.set_line_width(0.05);
+        renderer.present();
 
-    renderer.set_draw_color(Color::RGB(66, 66, 66));
-    renderer.draw_lines(&[Point::new(15, 1).scale(10),
-                      Point::new(5, 1).scale(10),
-                      Point::new(0, 10).scale(10),
-                      Point::new(5, 19).scale(10),
-                      Point::new(15, 19).scale(10),
-                      Point::new(20, 10).scale(10),
-                      Point::new(15, 1).scale(10)])
-        .unwrap();
+        let event_pump = sdl_context.event_pump().unwrap();
 
-    renderer.present();
+        Platform {
+            renderer: renderer,
+            event_pump: event_pump,
+        }
+    }
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    pub fn draw_hexagon(&mut self, x: usize, y: usize) {
+        self.renderer.set_draw_color(Color::RGB(66, 66, 66));
+        self.renderer
+            .draw_lines(&[Point::new(15, 1).scale(10),
+                          Point::new(5, 1).scale(10),
+                          Point::new(0, 10).scale(10),
+                          Point::new(5, 19).scale(10),
+                          Point::new(15, 19).scale(10),
+                          Point::new(20, 10).scale(10),
+                          Point::new(15, 1).scale(10)])
+            .unwrap();
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
+
+
+        self.renderer.present();
+    }
+
+    pub fn quit_on_keypress(&mut self) -> bool {
+        for event in self.event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } |
-                Event::KeyDown { /*keycode: Some(Keycode::Escape),*/ .. } => break 'running,
+                Event::KeyDown { /*keycode: Some(Keycode::Escape),*/ .. } => {return true;},
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
-    }
-}
 
-pub fn draw_hexagon(x: usize, y: usize) {
-    //TODO
+        false
+    }
 }
